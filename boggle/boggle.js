debed303bc79e7352d37e776fd1de8462b294bc3
John-Paul Meyer <johnnymeyer@gmail.com>
2016-01-21 21:23:51 +1100

    Added favicon. Made profile circle. Started a boggle page (no functionality yet)

---
+++ boggle/boggle.js
@@ -0,0 +1,27 @@
+/*
+var a1 = document.getElementById("a1").innerText;
+alert(a1);
+
+var dice = [['A', 'E', 'A', 'N', 'E', 'G'], ['W', 'N', 'G', 'E', 'E', 'H'],
+['A', 'H', 'S', 'P', 'C', 'O'], ['L', 'N', 'H', 'N', 'R', 'Z'],
+['A', 'S', 'P', 'F', 'F', 'K'], ['T', 'S', 'T', 'I', 'Y', 'D'],
+['O', 'B', 'J', 'O', 'A', 'B'], ['O', 'W', 'T', 'O', 'A', 'T'],
+['I', 'O', 'T', 'M', 'U', 'C'], ['E', 'R', 'T', 'T', 'Y', 'L'],
+['R', 'Y', 'V', 'D', 'E', 'L'], ['T', 'O', 'E', 'S', 'S', 'I'],
+['L', 'R', 'E', 'I', 'X', 'D'], ['T', 'E', 'R', 'W', 'H', 'V'],
+['E', 'I', 'U', 'N', 'E', 'S'], ['N', 'U', 'I', 'H', 'M', 'Qu']]
+
+function shuffle() {
+   alert("Entering shuffle");
+   $('#boggle-table tr').each(function(){
+      alert("outer loop");
+       $(this).find('td').each(function(){
+           alert($(this).);
+       })
+   })
+};
+
+shuffle();
+
+alert('this');
+*/
